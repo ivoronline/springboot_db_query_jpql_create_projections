6da849895a4f78ee5a0446ed41c3d09c09dc8238
ivoronline <ivoronline@gmail.com>
2021-11-26 09:34:33 +0100

    Commit

--- src/main/java/com/ivoronline/springboot_db_query_jpql_create_projections/controllers/MyController.java
+++ src/main/java/com/ivoronline/springboot_db_query_jpql_create_projections/controllers/MyController.java
@@ -1,24 +1,54 @@
 package com.ivoronline.springboot_db_query_jpql_create_projections.controllers;
 
 import com.ivoronline.springboot_db_query_jpql_create_projections.entities.Person;
-import com.ivoronline.springboot_db_query_jpql_create_projections.services.DBAccess;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 
 @RestController
 public class MyController {
 
-  @Autowired DBAccess dbAccess;
+  //PROPERTIES
+  @PersistenceContext EntityManager entityManager;
 
   //================================================================
   // RETURN ENTITY
   //================================================================
   @RequestMapping("ReturnEntity")
   Person returnEntity() {
-    Person person = dbAccess.returnEntity();
+
+    //CREATE QUERY
+    String select = "SELECT person FROM Person person WHERE person.name = :name";
+    Query  query  = entityManager.createQuery(select, Person.class);
+           query.setParameter("name", "John");
+
+    //SELECT ENTITY
+    Person person = (Person) query.getSingleResult();
+
+    //RETURN ENTITY
     return person;
+
+  }
+
+  //================================================================
+  // RETURN OBJECT ARRAY
+  //================================================================
+  @RequestMapping("ReturnObjectArray")
+  Object[] returnObjectArray() {
+
+    //CREATE QUERY
+    String select = "SELECT person.name, person.age FROM Person person WHERE person.name = :name";
+    Query  query  = entityManager.createQuery(select);
+           query.setParameter("name", "John");
+
+    //SELECT OBJECT ARRAY
+    Object[] objectArray  = (Object[]) query.getSingleResult();
+
+    //RETURN OBJECT ARRAY
+    return objectArray;
+
   }
 
   //================================================================
@@ -26,17 +56,20 @@ public class MyController {
   //================================================================
   @RequestMapping("ReturnScalar")
   Integer returnScalar() {
-    Integer age = dbAccess.returnScalar();
-    return  age;
+
+    //CREATE QUERY
+    String select = "SELECT person.age FROM Person person WHERE person.name = :name";
+    Query  query  = entityManager.createQuery(select);
+           query.setParameter("name", "John");
+
+    //SELECT SCALAR
+    Integer age = (Integer) query.getSingleResult();
+
+    //RETURN SCALAR
+    return age;
+
   }
-  //================================================================
-  // RETURN OBJECT ARRAY
-  //================================================================
-  @RequestMapping("returnObjectArray")
-  Object[] returnObjectArray() {
-    Object[] objectArray = dbAccess.returnObjectArray();
-    return   objectArray;
-  }
+
 
 }
 
